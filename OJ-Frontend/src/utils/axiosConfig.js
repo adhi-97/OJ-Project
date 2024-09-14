e0307e663f93c97bbeedbcb2099df962290a6c36
Adarsh <aadarshg.krishnan@gmail.com>
2024-09-14 12:26:21 +0530

    update

--- OJ-Frontend/src/utils/axiosConfig.js
+++ OJ-Frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/', // Your backend URL
+  baseURL: 'https://onlinejudge-oj.onrender.com/', // Your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
