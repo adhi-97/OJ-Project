dd281c29adbcf0ea699b24db4f1614bee6b0b127
Adarsh <aadarshg.krishnan@gmail.com>
2024-09-14 12:52:21 +0530

    update

--- OJ-Frontend/vite.config.js
+++ OJ-Frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensure this directory matches what Render expects
+    outDir: 'dist',
   },
-})
+  server: {
+    // Ensure that SPA routes are handled correctly
+    historyApiFallback: true,
+  },
+});
